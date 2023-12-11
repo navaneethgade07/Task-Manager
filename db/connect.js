3a138cbcd1218bd58451568bb30eb61e9c7b9650
Navaneeth Gade <navaneethgade07@gmail.com>
2023-12-11 21:53:17 +0530

    Comments

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-// const connectionString = "mongodb+srv://Navaneeth-07:Navaneeth-7@nodejsandexpressjsproje.jdtjsty.mongodb.net/?retryWrites=true&w=majority";
-
 // connecting our application to the database
 // hiding the connectionstring as url params using dotenv package while pushing to github
 const connectDB = (url)=>{
